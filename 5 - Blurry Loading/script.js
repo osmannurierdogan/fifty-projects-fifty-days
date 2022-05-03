728c81f16fb1819f47bb46e45bbe7b3e4d8b2762
Osman Nuri Erdoğan <osmerd04@gmail.com>
2022-05-03 21:15:45 +0300

    Project #5: Blurry Loading

---
+++ 5 - Blurry Loading/script.js
@@ -0,0 +1,23 @@
+const loadText = document.querySelector('.loading-text');
+const background = document.querySelector('.background');
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+let load = 0;
+
+let blurring = () => {
+    load++;
+    
+    if(load > 99){
+        clearInterval(interval);
+    }
+
+    loadText.innerHTML = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+let interval = setInterval(blurring, 30);
+
